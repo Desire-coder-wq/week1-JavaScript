9bb38370e76c1028850a45698354246c4488c74d
Desire <asinguradesirecomfort@gmail.com>
2025-07-30 08:09:12 +0300

    fisrt commit

---
+++ datatypesAndVariables.js
@@ -0,0 +1,68 @@
+/*
+Premetive Data types hold single values
+1. strings/Text
+2. numbers
+   intergers
+   big integer
+   decimals
+3. bolean
+   true/false
+
+4. undefined 
+   
+5. null
+   let x=null
+6. symbol
+   
+
+
+*/
+let productName ="timber"//strings we put them in quotes
+let productCost = 4500
+let transportIsNeeded = true
+let tableColor
+let discount = null
+console.log(productCost *2
+    
+)
+
+/*
+Non primitive Data type
+1. Array
+       is a list off item
+        [ number]
+        [string
+        ["sofa","table"]
+        let productList=[ sofa,table,cupboard]
+2. object
+        in javaScript everything is an object
+        {name:"sofa"
+        price: 2000
+        cloor:"blue"
+        }
+3. Data
+4. Function
+
+
+
+
+
+ */
+let productList = ["sofa","table","cupboard"]
+console.log(productList)
+
+let diningTableObject = { 
+ productName: "Dining table",
+ quantity: 2,
+ price : 3500,
+ totalCost : quantity * price
+}
+let quantity = 2
+let price = 3500
+let totalCost = quantity * price
+console.log(totalCost)
+
+let paymentType = "Cash"
+const VAT = 0.05 //const means value can't be changed
+const PI = 3.14
+console.log(paymentType)
---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+let a=5
+let b=10;
+let sum=a+b;
+console.log(sum)
+
+//comment
+/* multiple comment */
+
+
+
+console.log("The world of software engineering");
+let r=8*4
+let S=r*r;
+console.log(r);
